1ff9a719cc4e8739dbf6ff99347fc2e7067987cc
Mert Kahraman <73400722+TheDerebeyi@users.noreply.github.com>
2022-11-03 23:52:50 +0300

    Proje dosyası ekle.

---
+++ w5_2/Controllers/OgrenciController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using w5_2.Models;
+
+namespace w5_2.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        static List<Ogrenci> ogrenciList = new List<Ogrenci>();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Duzenle(string id)
+        {
+            TempData["Id"] = id;
+            return View(ogrenciList.Find(ogr => ogr.OgrNumara.Equals(id)));
+        }
+
+        public IActionResult Kayit()
+        {
+            return View();
+        }
+
+        public IActionResult Sil(string id)
+        {
+            TempData["Id"] = id;
+            return View(ogrenciList.Find(ogr => ogr.OgrNumara.Equals(id)));
+        }
+
+        public IActionResult Listele()
+        {
+            return View(ogrenciList);
+        }
+
+        [HttpPost]
+        public IActionResult Kaydedildi(Ogrenci ogr)
+        {
+            ogrenciList.Add(ogr);
+
+            return View("Listele", ogrenciList);
+        }
+
+        [HttpPost]
+        public IActionResult Silindi()
+        {
+            string tmp = TempData["Id"].ToString();
+            Ogrenci ogr = ogrenciList.Find(ogr => ogr.OgrNumara.Equals(tmp));
+            ogrenciList.Remove(ogr);
+
+            return View("Listele", ogrenciList);
+        }
+
+        [HttpPost]
+        public IActionResult Duzenlendi(Ogrenci ogr)
+        {
+            string tmp = TempData["Id"].ToString();
+            int index = ogrenciList.FindIndex(ogr => ogr.OgrNumara.Equals(tmp));
+            ogrenciList[index] = ogr;
+
+            return View("Listele", ogrenciList);
+        }
+    }
+}
---
+++ w5_2/Models/Ogrenci.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace w5_2.Models
+{
+    public class Ogrenci
+    {
+        [Display(Name ="Öğrenci Ad")]
+        public string OgrAd { get; set; }
+        [Display(Name = "Öğrenci Soyad")]
+        public string OgrSoyad { get; set; }
+        [Required]
+        [Display(Name = "Öğrenci Numara")]
+        public string OgrNumara { get; set; }
+        [Display(Name = "Öğrenci Yaş")]
+        public int OgrYas { get; set; }
+    }
+}
